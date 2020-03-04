cd6df2b9168bb32153aeb04824b68ac1c7dbcd98
camille griset <camilleflo@me.com>
2020-03-04 16:58:26 +0100

    ok

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,24 +1,22 @@
 class ReviewsController < ApplicationController
-
+before action: set_restaurant 
 	def new
-    # we need @restaurant in our `simple_form_for`
-		@restaurant = Restaurant.find(params[:restaurant_id])
+    # afficcher le formulaire
 		@review = Review.new
 	end
 end
 
-def create_review
+def create
     @review = Review.new(review_params)
-    # we need `restaurant_id` to associate review with corresponding restaurant
-    @restaurant = Restaurant.find(params[:restaurant_id])
+    # enregistrer la review en base
     @review.restaurant = @restaurant
     @review.save
     redirect_to restaurant_path(@restaurant)
-  end
+end
 
   private
 
-  def review_params
-    params.require(:review).permit(:content)
+  def set_restaurant
+    @restaurant = Restaurant.find(params[restaurant_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
 resources :restaurants do
-  members do
-  	get "review" => "restaurants#review"
+  resources :reviews, only:[:new, :create]
 end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
